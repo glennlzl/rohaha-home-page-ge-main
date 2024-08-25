0852366eb7383859ecea5d0e581cf798ab2c5c7b
glennlzl <glennli616@gmail.com>
2024-08-25 05:18:56 -0700

    asdfadsfasfadsf

--- src/Home/App.jsx
+++ src/Home/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CasePage from './CasePage'; // 新建的页面组件
 
 function App() {
-  console.log(Home); // 应该是一个函数或类组件，而不是 `undefined` 或其他类型
-
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/case/:id" component={CasePage} /> 动态路由
+        <Route exact path="/" >
+          <Home />
+        </Route>
+        <Route path="/case/:id" > 动态路由
+          <CasePage />
+        </Route>
       </Switch>
     </Router>
   );
